70177a6e121599e729ea178d885b00199dc359b3
Faith <flierheimer@gmail.com>
2024-11-17 09:57:13 -0700

    making a little ref table of colorado cities

---
+++ weather_data_dbt/models/reference/city_ref.sql
@@ -0,0 +1,31 @@
+{{ config(
+    materialized='table'
+) }}
+--grab all distinct city lats/longs ()
+with distinct_cities as (
+select
+    city_name,
+    city_lat,
+    city_long 
+from 
+    {{ source('weather_data', 'weather_data') }}
+group by
+    city_name,
+    city_lat,
+    city_long
+order by
+    city_name
+),
+
+final as (
+select
+    {{ dbt_utils.generate_surrogate_key(['city_name', 'city_lat', 'city_long']) }} as city_key,
+    *
+from
+    distinct_cities
+
+)
+
+select * from final
+
+
