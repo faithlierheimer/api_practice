868ca5fbe644c9907fbbd16f8dda91363fba9b46
faithlierheimer <flierheimer@gmail.com>
2025-03-15 11:19:22 -0600

    fix custom schema name macro

--- weather_data_dbt/macros/generate_schema_name.sql
+++ weather_data_dbt/macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%if target.name = 'dev'%}
+    {% if target.name == 'dev' %}
     {%- if custom_schema_name is none -%}
 
         {{ default_schema }}
